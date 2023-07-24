1a83f6a30b1d3c2a8ecf4583157ddfabbd2cd6fe
Firefly <firefly@ossii.com.tw>
2023-07-24 12:09:16 +0800

    Rewrite modaodfweb-generate-proof-key in C++
    * simplified the tooling a bit: use coolconfig for creating
      RSA key pairs for WOPI Proof headers.
    * got rid of a dependency: ssh-keygen, towards leaner docker images
    
    Signed-off-by: Firefly <firefly@ossii.com.tw>
    Change-Id: I161ae7f61f00e168e2861c9c5b5b019c319278d3

--- docker/from-packages/scripts/start-collabora-online.sh
+++ docker/from-packages/scripts/start-collabora-online.sh
@@ -8,7 +8,7 @@ if test "${DONT_GEN_SSL_CERT-set}" = set; then
 mkdir -p /tmp/ssl/
 cd /tmp/ssl/
 mkdir -p certs/ca
-openssl rand -writerand /opt/oxool/.rnd
+openssl rand -writerand /opt/modaodfweb/.rnd
 openssl genrsa -out certs/ca/root.key.pem 2048
 openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/ca/root.crt.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=Dummy Authority"
 mkdir -p certs/servers
@@ -21,9 +21,9 @@ else
 openssl req -key certs/servers/localhost/privkey.pem -new -sha256 -out certs/tmp/localhost.csr.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=${cert_domain}"
 fi
 openssl x509 -req -in certs/tmp/localhost.csr.pem -CA certs/ca/root.crt.pem -CAkey certs/ca/root.key.pem -CAcreateserial -out certs/servers/localhost/cert.pem -days 9131
-mv -f certs/servers/localhost/privkey.pem /etc/oxoolwsd/key.pem
-mv -f certs/servers/localhost/cert.pem /etc/oxoolwsd/cert.pem
-mv -f certs/ca/root.crt.pem /etc/oxoolwsd/ca-chain.cert.pem
+mv -f certs/servers/localhost/privkey.pem /etc/modaodfweb/key.pem
+mv -f certs/servers/localhost/cert.pem /etc/modaodfweb/cert.pem
+mv -f certs/ca/root.crt.pem /etc/modaodfweb/ca-chain.cert.pem
 fi
 
 # Disable warning/info messages of LOKit by default
@@ -36,27 +36,27 @@ if test -n "${aliasgroup1}" -o -n "${domain}" -o -n "${remoteconfigurl}"; then
     perl -w /start-collabora-online.pl || { exit 1; }
 fi
 if test -n "${username}"; then
-    perl -pi -e "s/<username (.*)>.*<\/username>/<username \1>${username}<\/username>/" /etc/oxoolwsd/oxoolwsd.xml
+    perl -pi -e "s/<username (.*)>.*<\/username>/<username \1>${username}<\/username>/" /etc/modaodfweb/modaodfweb.xml
 fi
 if test -n "${password}"; then
-    perl -pi -e "s/<password (.*)>.*<\/password>/<password \1>${password}<\/password>/" /etc/oxoolwsd/oxoolwsd.xml
+    perl -pi -e "s/<password (.*)>.*<\/password>/<password \1>${password}<\/password>/" /etc/modaodfweb/modaodfweb.xml
 fi
 if test -n "${server_name}"; then
-    perl -pi -e "s/<server_name (.*)>.*<\/server_name>/<server_name \1>${server_name}<\/server_name>/" /etc/oxoolwsd/oxoolwsd.xml
+    perl -pi -e "s/<server_name (.*)>.*<\/server_name>/<server_name \1>${server_name}<\/server_name>/" /etc/modaodfweb/modaodfweb.xml
 fi
 if test -n "${dictionaries}"; then
-    perl -pi -e "s/<allowed_languages (.*)>.*<\/allowed_languages>/<allowed_languages \1>${dictionaries:-de_DE en_GB en_US es_ES fr_FR it nl pt_BR pt_PT ru}<\/allowed_languages>/" /etc/oxoolwsd/oxoolwsd.xml
+    perl -pi -e "s/<allowed_languages (.*)>.*<\/allowed_languages>/<allowed_languages \1>${dictionaries:-de_DE en_GB en_US es_ES fr_FR it nl pt_BR pt_PT ru}<\/allowed_languages>/" /etc/modaodfweb/modaodfweb.xml
 fi
 
-# Restart when /etc/oxoolwsd/oxoolwsd.xml changes
+# Restart when /etc/modaodfweb/modaodfweb.xml changes
 [ -x /usr/bin/inotifywait -a -x /usr/bin/killall ] && (
-  /usr/bin/inotifywait -e modify /etc/oxoolwsd/oxoolwsd.xml
-  echo "$(ls -l /etc/oxoolwsd/oxoolwsd.xml) modified --> restarting"
-  /usr/bin/killall -1 oxoolwsd
+  /usr/bin/inotifywait -e modify /etc/modaodfweb/modaodfweb.xml
+  echo "$(ls -l /etc/modaodfweb/modaodfweb.xml) modified --> restarting"
+  /usr/bin/killall -1 modaodfweb
 ) &
 
 # Generate WOPI proof key
-oxoolwsd-generate-proof-key
+modaodfweblconfig generate-proof-key
 
-# Start oxoolwsd
-exec /usr/bin/oxoolwsd --version --o:sys_template_path=/opt/oxool/systemplate --o:child_root_path=/opt/oxool/child-roots --o:file_server_root_path=/usr/share/oxoolwsd --o:logging.color=false ${extra_params}
+# Start modaodfweb
+exec /usr/bin/modaodfweb --version --o:sys_template_path=/opt/modaodfweb/systemplate --o:child_root_path=/opt/modaodfweb/child-roots --o:file_server_root_path=/usr/share/modaodfweb --o:logging.color=false ${extra_params}
