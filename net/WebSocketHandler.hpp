CollaboraOnline/online

paste: cleanup excessive debugging.
We don't want to dump the whole paste buffer as hex, or indeed any
large mis-encoded string - instead crop to the problematic section
for easier debugging, and no performance hit.

Change-Id: I30518beea436895a42b3429c3cb6e16e2093cca9
Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

net/WebSocketHandler.hpp
@@ -778,10 +778,29 @@ protected:
             size_t offset = Util::isValidUtf8((unsigned char*)data, len);
             if (offset < len)
             {
-                std::string raw(data, len);
+                std::string hex, raw;
+                if (len < 256)
+                {
+                    raw = std::string(data, len);
+                    hex = "whole string:" + Util::dumpHex(raw);
+                }
+                else
+                {
+                    // 64 bytes before & after ...
+                    size_t cropstart, croplen;
+                    if (offset < 64)
+                        cropstart = 0;
+                    else
+                        cropstart = offset - 64;
+                    croplen = std::min<size_t>(len - cropstart, 128);
+                    assert (cropstart + croplen <= len);
+                    raw = std::string(data + cropstart, croplen);
+                    hex = "msg: "+ COOLProtocol::getAbbreviatedMessage(data, len) +
+                        " string region error at byte " + std::to_string(offset - cropstart) + ": " + Util::dumpHex(raw);
+                };
                 std::cerr << "attempting to send invalid UTF-8 message '" << raw << "' "
                           << " error at offset " << std::hex << "0x" << offset << std::dec
-                          << " bytes, string: " << Util::dumpHex(raw) << "\n";
+                          << " bytes, " << hex << "\n";
                 assert("invalid utf-8 - check Message::detectType()" && false);
             }
         }

MODAODF/modaodfweb

net/WebSocketHandler.hpp
@@ -778,10 +778,29 @@ protected:
             size_t offset = Util::isValidUtf8((unsigned char*)data, len);
             if (offset < len)
             {
-                std::string raw(data, len);
+                std::string hex, raw;
+                if (len < 256)
+                {
+                    raw = std::string(data, len);
+                    hex = "whole string:" + Util::dumpHex(raw);
+                }
+                else
+                {
+                    // 64 bytes before & after ...
+                    size_t cropstart, croplen;
+                    if (offset < 64)
+                        cropstart = 0;
+                    else
+                        cropstart = offset - 64;
+                    croplen = std::min<size_t>(len - cropstart, 128);
+                    assert (cropstart + croplen <= len);
+                    raw = std::string(data + cropstart, croplen);
+                    hex = "msg: "+ OXOOLProtocol::getAbbreviatedMessage(data, len) +
+                        " string region error at byte " + std::to_string(offset - cropstart) + ": " + Util::dumpHex(raw);
+                };
                 std::cerr << "attempting to send invalid UTF-8 message '" << raw << "' "
                           << " error at offset " << std::hex << "0x" << offset << std::dec
-                          << " bytes, string: " << Util::dumpHex(raw) << "\n";
+                          << " bytes, " << hex << "\n";
                 assert("invalid utf-8 - check Message::detectType()" && false);
             }
         }