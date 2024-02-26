CollaboraOnline/online

wasm: header include cleanup
Change-Id: Ia6396d9e10b9597602a88fced932c27c329b394b
Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

wasm/wasmapp.cpp
@@ -9,7 +9,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <config.h>
+
 #include "wasmapp.hpp"
+
+#include <FakeSocket.hpp>
+#include <Log.hpp>
+#include <COOLWSD.hpp>
+#include <Util.hpp>
 
 #include "base64.hpp"
 #include <emscripten/fetch.h>

MODAODF/modaodfweb

wasm/wasmapp.cpp
@@ -9,7 +9,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <config.h>
+
 #include "wasmapp.hpp"
+
+#include <FakeSocket.hpp>
+#include <Log.hpp>
+#include <OXOOLWSD.hpp>
+#include <Util.hpp>
 
 #include "base64.hpp"
 #include <emscripten/fetch.h>