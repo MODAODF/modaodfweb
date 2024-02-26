CollaboraOnline/online

annotation: restrict reply and modify while other comment is editing
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I3422b7d8d1a29b42d51eae69cee576a5ca2653a7

browser/src/layer/tile/CommentListSection.ts
@@ -567,6 +567,10 @@ export class CommentSection extends CanvasSectionObject {
 	}
 
 	public reply (annotation: any): void {
+		if (cool.Comment.isAnyEdit()) {
+			cool.CommentSection.showCommentEditingWarning();
+			return;
+		}
 		if ((<any>window).mode.isMobile()) {
 			var avatar = undefined;
 			var author = this.map.getViewName(this.sectionProperties.docLayer._viewId);
@@ -604,6 +608,10 @@ export class CommentSection extends CanvasSectionObject {
 	}
 
 	public modify (annotation: any): void {
+		if (cool.Comment.isAnyEdit()) {
+			cool.CommentSection.showCommentEditingWarning();
+			return;
+		}
 		if ((<any>window).mode.isMobile()) {
 			this.newAnnotationMobile(annotation, function(annotation: any) {
 				this.save(annotation);

MODAODF/modaodfweb

browser/src/layer/tile/CommentListSection.ts
@@ -567,6 +567,10 @@ export class CommentSection extends CanvasSectionObject {
 	}
 
 	public reply (annotation: any): void {
+		if (oxool.Comment.isAnyEdit()) {
+			oxool.CommentSection.showCommentEditingWarning();
+			return;
+		}
 		if ((<any>window).mode.isMobile()) {
 			var avatar = undefined;
 			var author = this.map.getViewName(this.sectionProperties.docLayer._viewId);
@@ -604,6 +608,10 @@ export class CommentSection extends CanvasSectionObject {
 	}
 
 	public modify (annotation: any): void {
+		if (oxool.Comment.isAnyEdit()) {
+			oxool.CommentSection.showCommentEditingWarning();
+			return;
+		}
 		if ((<any>window).mode.isMobile()) {
 			this.newAnnotationMobile(annotation, function(annotation: any) {
 				this.save(annotation);