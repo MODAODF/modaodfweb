CollaboraOnline/online

annotation: avoid exiting edit comment on autosave
show() can hide modify textbox, this broke comment autosave in impress,
but as precaution show doesn't need to be called on newly added autosaved comments

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I13fdeb265f46cb9b8786f62a2d6e7e5c4e492f00

browser/src/layer/tile/CommentSection.ts
@@ -715,6 +715,8 @@ export class Comment extends CanvasSectionObject {
 		if ((<any>window).mode.isMobile() && this.sectionProperties.container.parentElement === document.getElementById('document-container'))
 			this.sectionProperties.container.style.visibility = 'hidden';
 
+		if (cool.CommentSection.commentWasAutoAdded)
+			return;
 		if (this.sectionProperties.docLayer._docType === 'text')
 			this.showWriter();
 		else if (this.sectionProperties.docLayer._docType === 'presentation' || this.sectionProperties.docLayer._docType === 'drawing')

MODAODF/modaodfweb

browser/src/layer/tile/CommentSection.ts
@@ -715,6 +715,8 @@ export class Comment extends CanvasSectionObject {
 		if ((<any>window).mode.isMobile() && this.sectionProperties.container.parentElement === document.getElementById('document-container'))
 			this.sectionProperties.container.style.visibility = 'hidden';
 
+		if (oxool.CommentSection.commentWasAutoAdded)
+			return;
 		if (this.sectionProperties.docLayer._docType === 'text')
 			this.showWriter();
 		else if (this.sectionProperties.docLayer._docType === 'presentation' || this.sectionProperties.docLayer._docType === 'drawing')