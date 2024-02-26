CollaboraOnline/online

add hasWASMSupport to capabilities
so the launching site can query if wasm is enabled to see if it needs to
insert the required headers, so we can then in turn make it optional on
those being set to also require matching headers.

Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Change-Id: Icd73081809abb8098c21bc61a8357869db45ff6c

wsd/FileServer.cpp
@@ -1254,10 +1254,16 @@ void FileServerRequestHandler::preprocessFile(const HTTPRequest& request,
     // addHeader('Cross-Origin-Opener-Policy', 'same-origin');
     // addHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     // then we seem to have to have this to avoid
-    // NS_ERROR_DOM_CORP_FAILED
-    oss << "Cross-Origin-Opener-Policy: same-origin\r\n";
-    oss << "Cross-Origin-Embedder-Policy: require-corp\r\n";
-    oss << "Cross-Origin-Resource-Policy: cross-origin\r\n";
+    // NS_ERROR_DOM_CORP_FAILED.
+    //
+    // We expect richdocuments to require these headers if our
+    // capabilities shows hasWASMSupport
+    if (COOLWSD::WASMState != COOLWSD::WASMActivationState::Disabled)
+    {
+        oss << "Cross-Origin-Opener-Policy: same-origin\r\n";
+        oss << "Cross-Origin-Embedder-Policy: require-corp\r\n";
+        oss << "Cross-Origin-Resource-Policy: cross-origin\r\n";
+    }
 
     const bool wasm = (relPath.find("wasm") != std::string::npos);
     if (wasm)

MODAODF/modaodfweb

wsd/FileServer.cpp
@@ -1263,10 +1263,16 @@ void FileServerRequestHandler::preprocessFile(const HTTPRequest& request,
     // addHeader('Cross-Origin-Opener-Policy', 'same-origin');
     // addHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     // then we seem to have to have this to avoid
-    // NS_ERROR_DOM_CORP_FAILED
-    oss << "Cross-Origin-Opener-Policy: same-origin\r\n";
-    oss << "Cross-Origin-Embedder-Policy: require-corp\r\n";
-    oss << "Cross-Origin-Resource-Policy: cross-origin\r\n";
+    // NS_ERROR_DOM_CORP_FAILED.
+    //
+    // We expect richdocuments to require these headers if our
+    // capabilities shows hasWASMSupport
+    if (OXOOLWSD::WASMState != OXOOLWSD::WASMActivationState::Disabled)
+    {
+        oss << "Cross-Origin-Opener-Policy: same-origin\r\n";
+        oss << "Cross-Origin-Embedder-Policy: require-corp\r\n";
+        oss << "Cross-Origin-Resource-Policy: cross-origin\r\n";
+    }
 
     const bool wasm = (relPath.find("wasm") != std::string::npos);
     if (wasm)