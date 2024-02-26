CollaboraOnline/online

annotation: calc scroll vertical to make selected comment visible
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: Iaac78e7a61acd3600a4e6f88f222a60eb232f9d8

browser/src/control/Control.IdleHandler.ts
@@ -52,7 +52,9 @@ class IdleHandler {
 			}
 		}
 
-		if (window.mode.isDesktop() && !this.map.uiManager.isAnyDialogOpen()) {
+		// Ideally instead of separate isAnyEdit check here, we could check isAnyEdit inside isAnyDialogOpen,
+		// but unfortunatly that causes problem in _deactivate and unnecessary 'userinactive' message is sent
+		if (window.mode.isDesktop() && !this.map.uiManager.isAnyDialogOpen() && !cool.Comment.isAnyEdit()) {
 			this.map.focus();
 		}
 

MODAODF/modaodfweb

browser/src/control/Control.IdleHandler.ts
@@ -52,7 +52,9 @@ class IdleHandler {
 			}
 		}
 
-		if (window.mode.isDesktop() && !this.map.uiManager.isAnyDialogOpen()) {
+		// Ideally instead of separate isAnyEdit check here, we could check isAnyEdit inside isAnyDialogOpen,
+		// but unfortunatly that causes problem in _deactivate and unnecessary 'userinactive' message is sent
+		if (window.mode.isDesktop() && !this.map.uiManager.isAnyDialogOpen() && !oxool.Comment.isAnyEdit()) {
 			this.map.focus();
 		}
 