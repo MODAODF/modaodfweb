CollaboraOnline/online

cypress: add tests for tracked comments operation
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: If1a2bbe7d9d3ec7fa821db91aea1f7b3ed6f2c1e

cypress_test/integration_tests/desktop/writer/track_changes_spec.js
@@ -8,8 +8,11 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Track Changes', function (
 	var testFileName;
 
 	beforeEach(function () {
+		cy.viewport(1400, 600);
 		testFileName = helper.beforeAll(origTestFileName, 'writer');
 		desktopHelper.switchUIToCompact();
+		cy.cGet('#tb_editbar_item_sidebar').click(); // Hide sidebar.
+		desktopHelper.selectZoomLevel('50');
 	});
 
 	afterEach(function () {
@@ -42,7 +45,35 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Track Changes', function (
 
 	it('Accept All', function () {
 		helper.typeIntoDocument('Hello World');
+		for (var n = 0; n < 2; n++) {
+			cy.cGet('#tb_editbar_item_insertannotation').click();
+			cy.cGet('#annotation-modify-textarea-new').type('some text' + n);
+			cy.cGet('#annotation-save-new').click();
+			// Wait for animation
+			cy.wait(500);
+		}
 		enableRecord();
+
+		cy.cGet('#tb_editbar_item_insertannotation').click();
+		cy.cGet('#annotation-modify-textarea-new').type('some text2');
+		cy.cGet('#annotation-save-new').click();
+		cy.wait(500);
+		helper.typeIntoDocument('{home}');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2 .cool-annotation-menubar').click();
+		cy.cGet('body').contains('.context-menu-item', 'Remove').click();
+		cy.cGet('#comment-container-2').should('have.class','greyed');
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+
+		confirmChange('Accept All');
+		cy.cGet('#comment-container-1').should('contain','some text0');
+		cy.cGet('#comment-container-2').should('not.exist');
+		cy.cGet('#comment-container-3').should('contain','some text2');
+		cy.cGet('div.cool-annotation').should('have.length', 2);
+
 		helper.clearAllText();
 		helper.selectAllText();
 		cy.wait(500);
@@ -54,7 +85,36 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Track Changes', function (
 
 	it('Reject All', function () {
 		helper.typeIntoDocument('Hello World');
+		for (var n = 0; n < 2; n++) {
+			cy.cGet('#tb_editbar_item_insertannotation').click();
+			cy.cGet('#annotation-modify-textarea-new').type('some text' + n);
+			cy.cGet('#annotation-save-new').click();
+			// Wait for animation
+			cy.wait(500);
+		}
 		enableRecord();
+
+		cy.cGet('#tb_editbar_item_insertannotation').click();
+		cy.cGet('#annotation-modify-textarea-new').type('some text2');
+		cy.cGet('#annotation-save-new').click();
+		cy.wait(500);
+		helper.typeIntoDocument('{home}');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2 .cool-annotation-menubar').click();
+		cy.cGet('body').contains('.context-menu-item', 'Remove').click();
+		cy.cGet('#comment-container-2').should('have.class','greyed');
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+
+		confirmChange('Reject All');
+		cy.cGet('#comment-container-1').should('contain','some text0');
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2').should('not.have.class','greyed');
+		cy.cGet('#comment-container-3').should('not.exist');
+		cy.cGet('div.cool-annotation').should('have.length', 2);
+
 		helper.clearAllText();
 		helper.selectAllText();
 		cy.wait(500);
@@ -63,5 +123,58 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Track Changes', function (
 		helper.selectAllText();
 		cy.wait(500);
 		helper.expectTextForClipboard('Hello World');
+	});
+
+	it('Comment Undo-Redo', function () {
+		for (var n = 0; n < 2; n++) {
+			cy.cGet('#tb_editbar_item_insertannotation').click();
+			cy.cGet('#annotation-modify-textarea-new').type('some text' + n);
+			cy.cGet('#annotation-save-new').click();
+			// Wait for animation
+			cy.wait(500);
+		}
+		enableRecord();
+
+		cy.cGet('#tb_editbar_item_insertannotation').click();
+		cy.cGet('#annotation-modify-textarea-new').type('some text2');
+		cy.cGet('#annotation-save-new').click();
+		cy.wait(500);
+		helper.typeIntoDocument('{home}');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+
+		// simple undo
+		cy.cGet('#tb_editbar_item_undo').click();
+		cy.cGet('#comment-container-3').should('not.exist');
+		cy.cGet('div.cool-annotation').should('have.length', 2);
+
+		// simple redo
+		cy.wait(500);
+		cy.cGet('#tb_editbar_item_redo').click();
+		// cy.wait(500);
+		cy.cGet('#map').focus();
+		helper.typeIntoDocument('{home}');
+		cy.cGet('#comment-container-3').should('contain','some text2');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+
+		// undo removed comment
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2 .cool-annotation-menubar').click();
+		cy.cGet('body').contains('.context-menu-item', 'Remove').click();
+		cy.cGet('#comment-container-2').should('have.class','greyed');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+		cy.cGet('#tb_editbar_item_undo').click();
+
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2').should('not.have.class','greyed');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+
+		// redo
+		cy.cGet('#tb_editbar_item_redo').click();
+		cy.wait(500);
+
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2').should('have.class','greyed');
+		cy.cGet('div.cool-annotation').should('have.length', 3);
+
 	});
 });

MODAODF/modaodfweb

cypress_test/integration_tests/desktop/writer/track_changes_spec.js
@@ -8,8 +8,11 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Track Changes', function (
 	var testFileName;
 
 	beforeEach(function () {
+		cy.viewport(1400, 600);
 		testFileName = helper.beforeAll(origTestFileName, 'writer');
 		desktopHelper.switchUIToCompact();
+		cy.cGet('#tb_editbar_item_sidebar').click(); // Hide sidebar.
+		desktopHelper.selectZoomLevel('50');
 	});
 
 	afterEach(function () {
@@ -42,7 +45,35 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Track Changes', function (
 
 	it('Accept All', function () {
 		helper.typeIntoDocument('Hello World');
+		for (var n = 0; n < 2; n++) {
+			cy.cGet('#tb_editbar_item_insertannotation').click();
+			cy.cGet('#annotation-modify-textarea-new').type('some text' + n);
+			cy.cGet('#annotation-save-new').click();
+			// Wait for animation
+			cy.wait(500);
+		}
 		enableRecord();
+
+		cy.cGet('#tb_editbar_item_insertannotation').click();
+		cy.cGet('#annotation-modify-textarea-new').type('some text2');
+		cy.cGet('#annotation-save-new').click();
+		cy.wait(500);
+		helper.typeIntoDocument('{home}');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2 .oxool-annotation-menubar').click();
+		cy.cGet('body').contains('.context-menu-item', 'Remove').click();
+		cy.cGet('#comment-container-2').should('have.class','greyed');
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+
+		confirmChange('Accept All');
+		cy.cGet('#comment-container-1').should('contain','some text0');
+		cy.cGet('#comment-container-2').should('not.exist');
+		cy.cGet('#comment-container-3').should('contain','some text2');
+		cy.cGet('div.oxool-annotation').should('have.length', 2);
+
 		helper.clearAllText();
 		helper.selectAllText();
 		cy.wait(500);
@@ -54,7 +85,36 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Track Changes', function (
 
 	it('Reject All', function () {
 		helper.typeIntoDocument('Hello World');
+		for (var n = 0; n < 2; n++) {
+			cy.cGet('#tb_editbar_item_insertannotation').click();
+			cy.cGet('#annotation-modify-textarea-new').type('some text' + n);
+			cy.cGet('#annotation-save-new').click();
+			// Wait for animation
+			cy.wait(500);
+		}
 		enableRecord();
+
+		cy.cGet('#tb_editbar_item_insertannotation').click();
+		cy.cGet('#annotation-modify-textarea-new').type('some text2');
+		cy.cGet('#annotation-save-new').click();
+		cy.wait(500);
+		helper.typeIntoDocument('{home}');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2 .oxool-annotation-menubar').click();
+		cy.cGet('body').contains('.context-menu-item', 'Remove').click();
+		cy.cGet('#comment-container-2').should('have.class','greyed');
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+
+		confirmChange('Reject All');
+		cy.cGet('#comment-container-1').should('contain','some text0');
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2').should('not.have.class','greyed');
+		cy.cGet('#comment-container-3').should('not.exist');
+		cy.cGet('div.oxool-annotation').should('have.length', 2);
+
 		helper.clearAllText();
 		helper.selectAllText();
 		cy.wait(500);
@@ -63,5 +123,58 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Track Changes', function (
 		helper.selectAllText();
 		cy.wait(500);
 		helper.expectTextForClipboard('Hello World');
+	});
+
+	it('Comment Undo-Redo', function () {
+		for (var n = 0; n < 2; n++) {
+			cy.cGet('#tb_editbar_item_insertannotation').click();
+			cy.cGet('#annotation-modify-textarea-new').type('some text' + n);
+			cy.cGet('#annotation-save-new').click();
+			// Wait for animation
+			cy.wait(500);
+		}
+		enableRecord();
+
+		cy.cGet('#tb_editbar_item_insertannotation').click();
+		cy.cGet('#annotation-modify-textarea-new').type('some text2');
+		cy.cGet('#annotation-save-new').click();
+		cy.wait(500);
+		helper.typeIntoDocument('{home}');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+
+		// simple undo
+		cy.cGet('#tb_editbar_item_undo').click();
+		cy.cGet('#comment-container-3').should('not.exist');
+		cy.cGet('div.oxool-annotation').should('have.length', 2);
+
+		// simple redo
+		cy.wait(500);
+		cy.cGet('#tb_editbar_item_redo').click();
+		// cy.wait(500);
+		cy.cGet('#map').focus();
+		helper.typeIntoDocument('{home}');
+		cy.cGet('#comment-container-3').should('contain','some text2');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+
+		// undo removed comment
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2 .oxool-annotation-menubar').click();
+		cy.cGet('body').contains('.context-menu-item', 'Remove').click();
+		cy.cGet('#comment-container-2').should('have.class','greyed');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+		cy.cGet('#tb_editbar_item_undo').click();
+
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2').should('not.have.class','greyed');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+
+		// redo
+		cy.cGet('#tb_editbar_item_redo').click();
+		cy.wait(500);
+
+		cy.cGet('#comment-container-2').should('contain','some text1');
+		cy.cGet('#comment-container-2').should('have.class','greyed');
+		cy.cGet('div.oxool-annotation').should('have.length', 3);
+
 	});
 });