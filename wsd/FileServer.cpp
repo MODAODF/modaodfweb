CollaboraOnline/online

wsd: add logging token javascript errors
The "logging.html" end point was intended to capture
unhandled javascript exceptions from client side,
only corner cases, when websocket is disconnected or
very early errors before websocket is created.
So add a token to restrict access.

Change-Id: I64b7d7f3b3ddd83f044fea21dc07285b4465b3be
Signed-off-by: Henry Castro <hcastro@collabora.com>

wsd/FileServer.cpp
@@ -30,6 +30,7 @@
 #include <Poco/DateTimeFormatter.h>
 #include <Poco/Exception.h>
 #include <Poco/FileStream.h>
+#include <Poco/SHA1Engine.h>
 #include <Poco/Net/HTMLForm.h>
 #include <Poco/Net/HTTPBasicCredentials.h>
 #include <Poco/Net/HTTPCookie.h>
@@ -551,9 +552,20 @@ void FileServerRequestHandler::handleRequest(const HTTPRequest& request,
             const std::string coolLogging = config.getString("browser_logging", "false");
             if (coolLogging != "false")
             {
-                LOG_ERR(message.rdbuf());
-                socket->send(response);
-                return;
+                std::string token;
+                Poco::SHA1Engine engine;
+
+                engine.update(COOLWSD::LogToken);
+                std::getline(message, token, ' ');
+
+                if (Poco::DigestEngine::digestToHex(engine.digest()) == token)
+                {
+                    LOG_ERR(message.rdbuf());
+
+                    http::Response httpResponse(http::StatusCode::OK);
+                    socket->send(httpResponse);
+                    return;
+                }
             }
         }
 
@@ -1118,8 +1130,16 @@ void FileServerRequestHandler::preprocessFile(const HTTPRequest& request,
     Poco::replaceInPlace(preprocess, std::string("<!--%BRANDING_JS%-->"), brandJS);
     Poco::replaceInPlace(preprocess, std::string("<!--%CSS_VARIABLES%-->"), cssVarsToStyle(cssVars));
 
-    const auto coolLogging = stringifyBoolFromConfig(config, "browser_logging", false);
-    Poco::replaceInPlace(preprocess, std::string("%BROWSER_LOGGING%"), coolLogging);
+    if (config.getBool("browser_logging", false))
+    {
+        Poco::SHA1Engine engine;
+        engine.update(COOLWSD::LogToken);
+        Poco::replaceInPlace(preprocess, std::string("%BROWSER_LOGGING%"),
+                             Poco::DigestEngine::digestToHex(engine.digest()));
+    }
+    else
+        Poco::replaceInPlace(preprocess, std::string("%BROWSER_LOGGING%"), std::string());
+
     const auto groupDownloadAs = stringifyBoolFromConfig(config, "per_view.group_download_as", true);
     Poco::replaceInPlace(preprocess, std::string("%GROUP_DOWNLOAD_AS%"), groupDownloadAs);
     const unsigned int outOfFocusTimeoutSecs = config.getUInt("per_view.out_of_focus_timeout_secs", 60);

MODAODF/modaodfweb

wsd/FileServer.cpp
@@ -30,6 +30,7 @@
 #include <Poco/DateTimeFormatter.h>
 #include <Poco/Exception.h>
 #include <Poco/FileStream.h>
+#include <Poco/SHA1Engine.h>
 #include <Poco/Net/HTMLForm.h>
 #include <Poco/Net/HTTPBasicCredentials.h>
 #include <Poco/Net/HTTPCookie.h>
@@ -562,9 +563,20 @@ void FileServerRequestHandler::handleRequest(const HTTPRequest& request,
             const std::string oxoolLogging = config.getString("loleaflet_logging", "false");
             if (oxoolLogging != "false")
             {
-                LOG_ERR(message.rdbuf());
-                socket->send(response);
-                return;
+                std::string token;
+                Poco::SHA1Engine engine;
+
+                engine.update(OXOOLWSD::LogToken);
+                std::getline(message, token, ' ');
+
+                if (Poco::DigestEngine::digestToHex(engine.digest()) == token)
+                {
+                    LOG_ERR(message.rdbuf());
+
+                    http::Response httpResponse(http::StatusCode::OK);
+                    socket->send(httpResponse);
+                    return;
+                }
             }
         }
 
@@ -1129,8 +1141,16 @@ void FileServerRequestHandler::preprocessFile(const HTTPRequest& request,
     Poco::replaceInPlace(preprocess, std::string("<!--%BRANDING_JS%-->"), brandJS);
     Poco::replaceInPlace(preprocess, std::string("<!--%CSS_VARIABLES%-->"), cssVarsToStyle(cssVars));
 
-    const auto oxoolLogging = stringifyBoolFromConfig(config, "loleaflet_logging", false);
-    Poco::replaceInPlace(preprocess, std::string("%BROWSER_LOGGING%"), oxoolLogging);
+    if (config.getBool("loleaflet_logging", false))
+    {
+        Poco::SHA1Engine engine;
+        engine.update(OXOOLWSD::LogToken);
+        Poco::replaceInPlace(preprocess, std::string("%BROWSER_LOGGING%"),
+                             Poco::DigestEngine::digestToHex(engine.digest()));
+    }
+    else
+        Poco::replaceInPlace(preprocess, std::string("%BROWSER_LOGGING%"), std::string());
+
     const auto groupDownloadAs = stringifyBoolFromConfig(config, "per_view.group_download_as", true);
     Poco::replaceInPlace(preprocess, std::string("%GROUP_DOWNLOAD_AS%"), groupDownloadAs);
     const unsigned int outOfFocusTimeoutSecs = config.getUInt("per_view.out_of_focus_timeout_secs", 60);