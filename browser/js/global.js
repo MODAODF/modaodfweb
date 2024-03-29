CollaboraOnline/online

Fix &debug=true and &randomUser=true url parameters (backport)
Wait to do sidebar toggle
Fix parameter setting

Signed-off-by: Neil Guertin <neil.guertin@collabora.com>
Change-Id: Ib72b5ef4c2c31f7707a384b2d819d5cde57a796d

browser/js/global.js
@@ -89,8 +89,8 @@ window.app = {
 		var value = this.p.get(name);
 		return value === null ? '' : value;
 	}.bind(coolParams);
-	coolParams.set = function(name) {
-		this.p.set(name);
+	coolParams.set = function(name, value) {
+		this.p.set(name, value);
 	}.bind(coolParams);
 	global.coolParams = coolParams;
 

MODAODF/modaodfweb

browser/js/global.js
@@ -89,8 +89,8 @@ window.app = {
 		var value = this.p.get(name);
 		return value === null ? '' : value;
 	}.bind(oxoolParams);
-	oxoolParams.set = function(name) {
-		this.p.set(name);
+	oxoolParams.set = function(name, value) {
+		this.p.set(name, value);
 	}.bind(oxoolParams);
 	global.oxoolParams = oxoolParams;
 