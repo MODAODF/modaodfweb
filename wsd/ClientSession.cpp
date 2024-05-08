3872b76e5a757e8d7b01051669bc7a3baba0a370
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-05-08 06:04:54 -0400

    wsd: serve all files through sts
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit af984f849fe23c45f17ff352ca7a5457eec1adc6)
    
    Change-Id: I16b4d2a93ab5ec6f2a277b1b2e6be61c3515c2c8

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -30,6 +30,7 @@
 
 #include "DocumentBroker.hpp"
 #include "OXOOLWSD.hpp"
+#include "FileServer.hpp"
 #include <common/Common.hpp>
 #include <common/JsonUtil.hpp>
 #include <common/Log.hpp>
@@ -1928,6 +1929,7 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
 
                 const std::string fileName = Poco::Path(resultURL.getPath()).getFileName();
                 http::Response response(http::StatusCode::OK);
+                FileServerRequestHandler::hstsHeaders(response);
                 if (!fileName.empty())
                     response.set("Content-Disposition", "attachment; filename=\"" + fileName + '"');
                 response.setContentType("application/octet-stream");
@@ -2358,6 +2360,7 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
                 const std::string stringJSON = payload->jsonString();
 
                 http::Response httpResponse(http::StatusCode::OK);
+                FileServerRequestHandler::hstsHeaders(httpResponse);
                 httpResponse.set("Last-Modified", Util::getHttpTimeNow());
                 httpResponse.set("X-Content-Type-Options", "nosniff");
                 httpResponse.setBody(stringJSON, "application/json");
@@ -2386,6 +2389,7 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
                     std::string thumbnail(payload->data().data() + firstLineSize, payload->data().size() - firstLineSize);
 
                     http::Response httpResponse(http::StatusCode::OK);
+                    FileServerRequestHandler::hstsHeaders(httpResponse);
                     httpResponse.set("Last-Modified", Util::getHttpTimeNow());
                     httpResponse.set("X-Content-Type-Options", "nosniff");
                     httpResponse.setBody(thumbnail, "image/png");
