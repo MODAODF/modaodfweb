a741345631ef3071a5240815f54d5b0dd823a04a
Firefly <firefly@ossii.com.tw>
2024-06-20 18:48:08 +0800

    Unblock Notebook bar and Sidebar.
    Signed-off-by: Firefly <firefly@ossii.com.tw>
    Change-Id: I0cc1af97ba07eb0d8e4df4ea6c7170612a856e80

--- browser/src/docstate.js
+++ browser/src/docstate.js
@@ -10,8 +10,6 @@
  */
 
 window.app = { // Shouldn't have any functions defined.
-	dontUseNotebookbar: true,
-	dontUseSidebar: true,
 	definitions: {}, // Class instances are created using definitions under this variable.
 	dpiScale: window.devicePixelRatio,
 	roundedDpiScale: Math.round(window.devicePixelRatio),
