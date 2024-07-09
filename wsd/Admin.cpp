Rash419/online

fix: don't wait for migrate msg if indirection server closes the connection because deployment gets deleted
- if deployment gets deleted than there is no where to migrate the
documents in such cases indirection will close the monitor connection
but we need to set the _migrateMsgReceived flag so that docBroker don't
wait for the message and continue to cleanup the document

Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
Change-Id: Ia2732740927ea16e368f91bea7421576a3cf2beb

wsd/Admin.cpp
@@ -715,6 +715,11 @@ void Admin::pollingThread()
         }
         poll(std::chrono::seconds(1));
     }
+
+    // if monitor closes early we set the _migrateMsgReceived for each docbroker
+    // so that docbroker can cleanup the documents
+    if (_closeMonitor)
+        COOLWSD::setAllMigrationMsgReceived();
 }
 
 void Admin::modificationAlert(const std::string& docKey, pid_t pid, bool value){

MODAODF/modaodfweb

wsd/Admin.cpp
@@ -737,6 +737,11 @@ void Admin::pollingThread()
         }
         poll(closeMonitorMsgTimeout - elapsedMicroS);
     }
+
+    // if monitor closes early we set the _migrateMsgReceived for each docbroker
+    // so that docbroker can cleanup the documents
+    if (_closeMonitor)
+        OXOOLWSD::setAllMigrationMsgReceived();
 }
 
 void Admin::modificationAlert(const std::string& dockey, pid_t pid, bool value){